57122c5dc2cca826d612959fe3da34a74a43664c
Mauricio Rojas <mroja001@usmovnmroja001.attlocal.net>
2017-02-10 14:19:53 -0600

    healthchecking vsd and installing license

--- foo.py
+++
@@ -1,6 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('cspuser', type=str)
-parser.add_argument('passwd', type=str)
-args =  parser.parse_args()
-print "\"vsd user is %s\"" % args.cspuser
